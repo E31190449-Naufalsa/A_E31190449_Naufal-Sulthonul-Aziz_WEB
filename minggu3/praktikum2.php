f28ddc2fdee39e6a3464e0b90518ab4caefd0f8c
E31190449-Naufalsa <65713208+E31190449-Naufalsa@users.noreply.github.com>
2021-03-13 09:29:04 +0700

    Update praktikum2.php

--- minggu3/praktikum2.php
+++ minggu3/praktikum2.php
@@ -4,6 +4,8 @@ class Car {
 
 	//private
 	private $model;
+	// agar dapat menampilkan nilai dari property private maka nilai harus di deklarasikan terlebih dulu, contoh
+	// private $model="Marcedes Benz";
 
 	public function getModel()
 	{
