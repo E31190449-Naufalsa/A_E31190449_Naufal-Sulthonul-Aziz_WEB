99592a81a0e532fb1676fc7c3a4e5cfa1014f9a5
E31190449-Naufalsa <65713208+E31190449-Naufalsa@users.noreply.github.com>
2021-03-15 11:05:55 +0700

    E31190449

---
+++ minggu6/CodeIgniter/application/views/view_kode4.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Controller dan View lebih dari 1 Variabel</title>
+</head>
+<body>
+	<h2>Mengirim Data dari Controller ke View</h2>
+
+	<!--Memanggil Variabel 1-->
+	variabel: <?php echo variabel1; ?></br>
+
+	<!--Memanggil variabel 2-->
+	variabel2: <?php variabel2; ?></br>
+</body
+</html>
